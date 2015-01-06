9f7c35a8f56affefdbf4d2bfd12d1ec5773aa5e7
aliakseihaidukou <aliaksei_haidukou@epam.com>
2015-01-06 16:24:29 +0300

    [Test] - test PentahoSecurityContextHolderStrategyTest for multi threading leak

--- core/test-src/org/pentaho/platform/engine/security/PentahoSecurityContextHolderStrategyTest.java
+++ core/test-src/org/pentaho/platform/engine/security/PentahoSecurityContextHolderStrategyTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 import org.springframework.security.Authentication;
 import org.springframework.security.context.SecurityContext;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
@@ -34,5 +39,53 @@ public class PentahoSecurityContextHolderStrategyTest {
       thread.wait();
     }
     assertSame( authentication, strategy.getContext().getAuthentication() );
+  }
+
+  @Test
+  public void testMultipleRequests() throws Exception {
+    final PentahoSecurityContextHolderStrategy strategy = new PentahoSecurityContextHolderStrategy();
+    SecurityContext context = strategy.getContext();
+    assertNotNull( context );
+    final Authentication authentication1 = mock( Authentication.class );
+    context.setAuthentication( authentication1 );
+
+    assertSame( authentication1, strategy.getContext().getAuthentication() );
+
+    final AtomicInteger hasErrors = new AtomicInteger( 0 );
+
+    final Runnable runnable = new Runnable() {
+      @Override
+      public void run() {
+        if ( !authentication1.equals( strategy.getContext().getAuthentication() ) ) {
+          hasErrors.incrementAndGet();
+          fail();
+        }
+        Authentication authentication2 = mock( Authentication.class );
+        strategy.getContext().setAuthentication( authentication2 );
+        if ( !authentication2.equals( strategy.getContext().getAuthentication() ) ) {
+          hasErrors.incrementAndGet();
+          fail();
+        }
+      }
+    };
+
+    ThreadFactory tf = new ThreadFactory() {
+      @Override
+      public Thread newThread( Runnable r ) {
+        return new Thread( r );
+      }
+    };
+
+    ExecutorService executor = Executors.newSingleThreadExecutor( tf );
+    for ( int i = 0; i < 5; i++ ) {
+      executor.execute( runnable );
+    }
+    executor.shutdown();
+    while ( !executor.isTerminated() ) {
+      Thread.sleep( 100 );
+    }
+
+    assertSame( 0, hasErrors.get() );
+    assertSame( authentication1, strategy.getContext().getAuthentication() );
   }
 }
